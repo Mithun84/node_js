745d7e8d142b732246d6faf89e438409ce4e74d5
Mithun Shaw <73377949+Mithun84@users.noreply.github.com>
2022-10-06 20:45:12 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name:'Mithun Shaw',email:'mithun@gmail.com',contact:'7278575888'},
+    {name:'Mithun Shaw',email:'mithun@gmail.com',contact:'7278575888'},
+    {name:'Mithun Shaw',email:'mithun@gmail.com',contact:'7278575888'}
+
+]
+module.exports = data;
---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const path = require('path');
+
+const dirPath = path.join(__dirname,'files');
+
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(dirPath+"/hello"+i+".txt","a simple text file");
+// }
+
+fs.readdir(dirPath,(err,files)=>{
+
+    files.forEach((item)=>{
+        console.warn("file name is",item)
+        
+    })
+
+})
